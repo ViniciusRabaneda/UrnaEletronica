c684c8bf4527d2f0559987588c3aaab417f92b5b
Vinicius Rabaneda <vinirabaneda@hotmail.com>
2024-07-31 22:26:20 -0300

    importando dados do etapa.js

--- script.js
+++ script.js
@@ -28,7 +28,31 @@ function comecarEtapa() {
 }
 
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero){
+            return true;
+        }else{
+            return false;
+        }
+    });
+    if(candidato.length>0){
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display ='block';
+        descricao.innerHTML =`Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
 
+        let fotosHtml ='';
+        for(let i in candidato.fotos){
+            fotosHtml+= `<div class="d-1-image"><img src ="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda} </div>`;
+        }
+
+        laeral.innerHTML = fotosHtml;
+    }else{
+        seuVotoPara.style.display='block';
+        aviso.style.display ='block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
 }
 
 function clicou(n) {
@@ -39,6 +63,7 @@ function clicou(n) {
         elNumero.classList.remove('pisca');
         if (elNumero.nextElementSibling !== null) {
             elNumero.nextElementSibling.classList.add('pisca'); // pega o item mais proximo
+        }else{
             atualizaInterface();
         }
     }
